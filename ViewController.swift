2a31846c39ca6086ed3b4b4f2df1c54f81e73488
Yasiru Jayatissa <110222524+yasirujayatissa@users.noreply.github.com>
2022-08-19 11:12:42 +0530

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsingViews
+//
+//  Created by D.Yasiru Jayatissa on 2022-08-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
